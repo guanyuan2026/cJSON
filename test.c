e6e8f8c0fa2c4ae4004fa43aaa9ae9333a836d6a
Vincent Yua <15633162026@163.com>
2020-09-24 06:21:15 +0800

    验证可正常解析

--- test.c
+++ test.c
@@ -2,7 +2,76 @@
 #include <stdlib.h>
 #include "cJSON.h"
 
+#include <string.h>
+
+
+
 int main(void)
 {
-	;
+	char *Output2 = "{\"state\":\"1\"}";
+
+	//从缓冲区中解析出JSON结构
+	cJSON * cj= NULL;
+	cJSON * item = NULL;
+	cJSON * object = NULL;
+	cJSON * arrayItem = NULL;
+	
+	
+	cj = cJSON_Parse(Output2);
+	if(!cj)
+    {printf("2");
+        printf("Error before: [%s]\n",cJSON_GetErrorPtr());
+        return -1 ;
+    }
+	
+    item = cJSON_GetObjectItem(cj,"state");
+    if(item == NULL)
+    {
+		printf("Error before: [%s]\n",cJSON_GetErrorPtr());  
+        cJSON_Delete(cj);
+        return -1;
+    }
+	printf("%s \n",item->valueint);
+	printf("%s \n",item->valuestring);
+
+	const char *Output1 = "{\"state\":\"2\",\"list\":[{\"cmd\":\"device\"},{\"cmd\":\"resetfingerprint\"}]}";
+	
+	cj = cJSON_Parse(Output1);
+	if(!cj)
+    {
+        printf("Error before: [%s]\n",cJSON_GetErrorPtr());
+        return -1 ;
+    }
+	
+    item = cJSON_GetObjectItem(cj,"state");
+    if(item == NULL)
+    {
+		printf("Error before: [%s]\n",cJSON_GetErrorPtr());  
+        cJSON_Delete(cj);
+        return -1;
+    }
+	printf("%s \n",item->valueint);
+	printf("%s \n",item->valuestring);
+	
+	arrayItem = cJSON_GetObjectItem(cj,"list");
+    if(!arrayItem)
+    {
+        printf("cjson_parse failed \n");
+        return -1 ;
+    }
+	int size=cJSON_GetArraySize(arrayItem);  
+	for(int i=0;i<size;i++)
+	{
+		object=cJSON_GetArrayItem(arrayItem,i);
+		item=cJSON_GetObjectItem(object,"cmd");
+		if(item!=NULL)  
+		{  
+			printf("cJSON_GetObjectItem: type=%d, string is %s, value is %s\n",item->type,item->string,item->valuestring);   
+		}
+	}
+	
+	
+    cJSON_Delete(cj);
+	
+
 }
